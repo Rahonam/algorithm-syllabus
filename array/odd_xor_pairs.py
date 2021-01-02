7a8b2c938ff92e44e69452bd44405892de329c3d
Manohar Kumar <manoharkumar371@gmail.com>
2021-01-02 11:14:38 +0530

    Find the number of pairs with odd XOR

---
+++ array/odd_xor_pairs.py
@@ -0,0 +1,69 @@
+
+def odd_xor(arr: list):
+    """
+    Find the number of pairs with odd XOR 
+
+    For bits:
+        XOR is 0 if same bits, otherwise 1
+
+    using: iteration
+
+    Args:
+        arr: array of integers
+    
+    Returns:
+        int: count of odd XOR pairs
+    """
+    if len(arr) == 1:
+        return 1
+
+    count = 0
+    left_index = 0
+    while left_index < len(arr) - 1:
+        right_index = left_index + 1
+        while right_index < len(arr):
+            xor = (arr[left_index] | arr[right_index]) & (~arr[left_index] | ~arr[right_index])
+            xor = (arr[left_index] | arr[right_index]) & (~arr[left_index] | ~arr[right_index])
+            if xor % 2 != 0:
+                count += 1
+            right_index += 1
+        left_index += 1
+
+    return count
+
+
+def oddXor(arr: list):
+    """
+    Find the number of pairs with odd XOR 
+
+    For bits:
+        XOR is 0 if same bits, otherwise 1
+
+    For numbers:
+        XOR is odd if one number is odd and another is even, otherwise even
+
+    using: iteration
+
+    Args:
+        arr: array of integers
+    
+    Returns:
+        int: count of odd XOR pairs
+    """
+    if len(arr) == 1:
+        return 1
+
+    odd_count = 0
+    even_count = 0
+    for i in arr:
+        if i % 2 == 0:
+            even_count += 1
+        else:
+            odd_count += 1
+
+    return odd_count * even_count
+
+
+print(oddXor([3]))
+print(oddXor([3,2,1]))
+print(oddXor([3,6,9,4]))
